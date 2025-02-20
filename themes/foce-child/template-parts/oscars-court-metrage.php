e7bccc544252d5cde4479f538f14d4f819ff8b3c
Jose Portilla <joseportilla@gmail.com>
2025-02-20 14:41:36 +0100

    first commit

---
+++ themes/foce-child/template-parts/oscars-court-metrage.php
@@ -0,0 +1,5 @@
+<section class="oscars-court-metrage">
+    <div>
+        <img class="img-section-court-metrage" src="http://koukaki.local/wp-content/uploads/2025/02/Ocars-court-metrage.png" alt="Oscars Court Métrage" />
+    </div>
+</section>
